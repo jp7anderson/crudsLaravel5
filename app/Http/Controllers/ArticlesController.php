56bd599f07b7d15d83a8d902a3a0a4f470cefb3a
Anderson Dario <anderson@jp7.com.br>
2015-05-28 18:39:50 -0300

    Finished controller struct

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -10,7 +10,7 @@ class ArticlesController extends Controller {
 
 	public function __construct() {
 		//App\Http\Controllers\Controller -- está setado no App\Http\Kernel.php
-		$this->middleware('auth', ['only' => 'create']);
+		$this->middleware('auth', ['except' => ['index', 'show']]);
 	}
 
 	public function index() {
@@ -25,26 +25,50 @@ class ArticlesController extends Controller {
 	}
 
 	public function create() {
-		return view('articles.create');
+		$tags = \App\Tag::lists('name', 'id');
+
+		return view('articles.create', compact('tags'));
 	}
 
 	public function store(ArticleRequest $request) {
-		$article = new Article($request->all());
-		\Auth::user()->articles()->save($article);
+		/*
+		 * $article = new Article($request->all()); dando um create com os dados do input
+		 * \Auth::user()->articles()->save($article); e salvando
+		 */
+
+		$this->createArticle($request);
+
+		flash()->success('Your article has been created!');
 
 		return redirect('articles');
 	}
 
 	public function edit(Article $article) {
 		//a pesquisa pelo id é feita no App\Providers\RouteServiceProvider
-		return view('articles.edit', compact('article'));
+		$tags = \App\Tag::lists('name', 'id');
+
+		return view('articles.edit', compact('article', 'tags'));
 	}
 
 	public function update(Article $article, ArticleRequest $request) {
 		//a pesquisa pelo id é feita no App\Providers\RouteServiceProvider
 		$article->update($request->all());
 
+		$this->syncTags($article, $request->input('tag_list'));
+
 		return redirect('articles');
+	}
+
+	private function syncTags(Article $article, array $tags) {
+		$article->tags()->sync($tags);
+	}
+
+	private function createArticle(ArticleRequest $request) {
+		$article = \Auth::user()->articles()->create($request->all());
+
+		$this->syncTags($article, $request->input('tag_list'));
+
+		return $article;
 	}
 
 }
